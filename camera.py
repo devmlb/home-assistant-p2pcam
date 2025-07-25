acf54a1cb5c36303ce8d428e18b41aeaede9f38c
devmlb <97614168+devmlb@users.noreply.github.com>
2025-07-25 14:11:09 +0200

    Cleaning

--- camera.py
+++ camera.py
@@ -8,27 +8,12 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 
-CONF_HORIZONTAL_FLIP = 'horizontal_flip'
-CONF_VERTICAL_FLIP = 'vertical_flip'
-CONF_TIMESTAMP = 'timestamp'
-
-DEFAULT_NAME = 'p2pcam'
-DEFAULT_HORIZONTAL_FLIP = 0
-DEFAULT_VERTICAL_FLIP = 0
-DEFAULT_TIMESTAMP = 0
-
-REQUIREMENTS = ['opencv-python==4.0.0.21']
+DEFAULT_NAME = "p2pcam"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Optional(CONF_HOST): cv.string,
-    vol.Optional(CONF_IP_ADDRESS): cv.string,
-    vol.Optional(CONF_HORIZONTAL_FLIP, default=DEFAULT_HORIZONTAL_FLIP):
-        vol.All(vol.Coerce(int), vol.Range(min=0, max=1)),
-    vol.Optional(CONF_VERTICAL_FLIP, default=DEFAULT_VERTICAL_FLIP):
-        vol.All(vol.Coerce(int), vol.Range(min=0, max=1)),
-    vol.Optional(CONF_TIMESTAMP, default=DEFAULT_TIMESTAMP):
-        vol.All(vol.Coerce(int), vol.Range(min=0, max=1)),
+    vol.Optional(CONF_IP_ADDRESS): cv.string
 })
 
 
@@ -47,10 +32,6 @@ class P2PCam(Camera):
         self._target_ip = config.get(CONF_IP_ADDRESS)
 
         self.camera = p2pcam_req.P2PCam(self._host_ip, self._target_ip)
-        self.camera.horizontal_flip = (bool(config.get(CONF_HORIZONTAL_FLIP)) == 1)
-        self.camera.vertical_flip = (int(config.get(CONF_VERTICAL_FLIP)) == 1)
-        self.camera.vertical_flip = (int(config.get(CONF_VERTICAL_FLIP)) == 1)
-        self.camera.addTimeStamp = (int(config.get(CONF_TIMESTAMP)) == 1)
 
     async def async_camera_image(self):
         return self.camera.retrieveImage()
