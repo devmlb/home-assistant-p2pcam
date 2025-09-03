caceff4ac02e90fb3c51575609822021d3289121
devmlb <97614168+devmlb@users.noreply.github.com>
2025-09-03 15:11:46 +0000

    Removed unnecessary function and cleaned up the code

--- __init__.py
+++ __init__.py
@@ -1,54 +1,31 @@
 """Initialize the P2PCam integration package"""
-import logging
+
 
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-
+import logging
 from .const import DOMAIN, PLATFORMS
+
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup(hass: HomeAssistant, config: ConfigEntry):
-    """Initialisation de l'intégration"""
-    _LOGGER.info(
-        "Initializing %s integration with plaforms: %s with config: %s",
-        DOMAIN,
-        PLATFORMS,
-        config,
-    )
-
-    # Mettre ici un eventuel code permettant l'initialisation de l'intégration
-    # Ca peut être une connexion sur le Cloud qui fournit les données par ex
-    # (pas nécessaire pour le tuto)
-
-    # L'argument config contient votre fichier configuration.yaml
-    my_config = config.get(DOMAIN)  # pylint: disable=unused-variable
-
-    # Return boolean to indicate that initialization was successful.
-    return True
-
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Creation des entités à partir d'une configEntry"""
-
-    _LOGGER.debug(
-        "Appel de async_setup_entry entry: entry_id='%s', data='%s'",
-        entry.entry_id,
-        entry.data,
-    )
+    """Create entities from a configEntry"""
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
+    # Each platform must complete the setup
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    # Enregistrement de l'écouteur de changement 'update_listener'
+    # Registration of the update_listener function
     entry.async_on_unload(entry.add_update_listener(update_listener))
 
     return True
 
 
 async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
-    """Fonction qui force le rechargement des entités associées à une configEntry"""
+    """Force the reloading of entities associated with a configEntry"""
+
     await hass.config_entries.async_reload(entry.entry_id)
