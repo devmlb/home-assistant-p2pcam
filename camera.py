8ac77eb154a9393781ac406cdf4ce2ebd3619844
devmlb <97614168+devmlb@users.noreply.github.com>
2025-09-04 19:03:48 +0000

    Multi-device streaming fix

--- camera.py
+++ camera.py
@@ -9,6 +9,7 @@ from homeassistant.const import CONF_NAME
 from homeassistant.components.camera import Camera
 from p2pcam import P2PCam
 import logging
+import threading
 from .const import DOMAIN, CONF_HOST_IP, CONF_CAMERA_IP, ATTR_HORIZONTAL, ATTR_VERTICAL, ATTR_TIMESTAMP
 
 
@@ -33,6 +34,13 @@ class P2PCamera(Camera):
         self._cam_id = cam_id
         # P2PCam object
         self._cam = P2PCam(host_ip, camera_ip)
+        self._cam.NB_FRAGMENTS_TO_ACCUMULATE = 20
+        # Image buffer for real time streaming
+        self._latest_image = None
+        self._image_lock = threading.Lock()
+        self._stop_thread = False
+        self._thread = threading.Thread(target=self._update_image_loop, daemon=True)
+        self._thread.start()
 
     @property
     def device_info(self) -> DeviceInfo:
@@ -45,18 +53,40 @@ class P2PCamera(Camera):
             model=self._device_id,
         )
 
+    def _update_image_loop(self) -> None:
+        """Continuously retrieve the latest image from the camera"""
+
+        # We must use this loop because if several devices stream the camera feed
+        # at the same time, Home Assistant runs image retrieval in parallel which
+        # overloads the camera and causes the feed to stop
+        # The following code allows us to use a single image retrieval loop and store
+        # the latest available image which will then be distributed to all devices
+        while not self._stop_thread:
+            try:
+                # Setting a few parameters before taking the photo
+                self._cam.horizontal_flip = self._attrs[ATTR_HORIZONTAL]
+                self._cam.vertical_flip = self._attrs[ATTR_VERTICAL]
+                self._cam.add_timestamp = self._attrs[ATTR_TIMESTAMP]
+                # Image capture
+                image = self._cam.retrieveImage()
+                # Storing the latest image
+                with self._image_lock:
+                    self._latest_image = image
+            except Exception as e:
+                _LOGGER.error("Cannot retrieve the video stream from the camera: %s", e)
+
     def camera_image(self, width=None, height=None) -> bytes | None:
-        """Return an image retrieved by the P2PCam module"""
+        """Return the latest available image from the camera"""
 
-        # Set some settings before taking the picture
-        self._cam.horizontal_flip = self._attrs[ATTR_HORIZONTAL]
-        self._cam.vertical_flip = self._attrs[ATTR_VERTICAL]
-        self._cam.add_timestamp = self._attrs[ATTR_TIMESTAMP]
+        with self._image_lock:
+            return self._latest_image
 
-        try:
-            return self._cam.retrieveImage()
-        except Exception as e:
-            _LOGGER.error("Cannot retrieve the video stream from the camera:", e)
+    def __del__(self) -> None:
+        """"Properly stop the loop image retrieval"""
+
+        self._stop_thread = True
+        if hasattr(self, "_thread"):
+            self._thread.join(timeout=1)
 
 
 async def async_setup_entry(
